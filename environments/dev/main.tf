33c9a5331540ea919ad2845c5475eab5f2db935a
vish <admin@Admins-MacBook-Pro.local>
2024-09-22 14:57:51 -0400

    provision vpc

---
+++ environments/dev/main.tf
@@ -0,0 +1,9 @@
+module "vpc" {
+  source              = "../../modules/vpc"
+  cidr_block          = "10.0.0.0/16"
+  public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+  secure_subnet_cidrs  = ["10.0.5.0/24", "10.0.6.0/24"]
+  azs                 = ["us-east-1a", "us-east-1b"]
+  name                = "dev-pet-clinic"
+}
